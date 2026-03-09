d8a98f7631e2e6468fc04a0f5e5d0fac2076ca4e
smethre <smethre@sdl.com>
2026-03-09 19:30:12 +0530

    Updated Environment varaibles

--- example/src/environments/environment.ts
+++ example/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  clientId: '8bfa0eab-6b7a-4789-adf5-f20c03304b67',
+  clientId: '',
   issuer: 'https://domain.com/access-management/connect',
   openApiUrl: 'https://domain.com/api/v3.0',
   redirectUri: window.location.origin,
